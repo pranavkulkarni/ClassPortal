ee35bc99917381d656ce1afb510fc0982f21af42
pranavkulkarni <PranavKulkarni@Pranavs-MacBook-Pro-2.local>
2016-02-16 22:50:18 -0500

    seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,4 @@
 # Examples:
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-   Admin.create(id: "1", name: 'Pranav K', email: 'pkulkar5@ncsu.edu', password: '1', created_at: "2016-02-12 23:11:54.405191", updated_at: "2016-02-14 01:12:35.667767")
+   Admin.create(id: "1", name: "Pranav K", email: "pkulkar5@ncsu.edu", password: "1", created_at: "2016-02-12 23:11:54.405191", updated_at: "2016-02-14 01:12:35.667767")
