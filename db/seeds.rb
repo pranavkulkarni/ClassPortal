dd56a06181d250d95ea2b3345ae4f25897b0ef15
Isha Bobra <ibobra@ncsu.edu>
2016-02-20 22:00:51 -0500

    Changes for Heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,9 +4,8 @@
 # Examples:
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-<<<<<<< HEAD
-#   Admin.create(id: "1", name: "Pranav K", email: "pkulkar5@ncsu.edu", password: "1", created_at: "2016-02-12 23:11:54.405191", updated_at: "2016-02-14 01:12:35.667767")
-=======
+   Admin.create(id: "1", name: "Pranav K", email: "pkulkar5@ncsu.edu", password: "1", created_at: "2016-02-12 23:11:54.405191", updated_at: "2016-02-14 01:12:35.667767")
+   Student.create(id: "1", name: "Vivek R", email: "v@ncsu.edu", password: "1", created_at: "2016-02-12 23:11:54.405191", updated_at: "2016-02-14 01:12:35.667767")
+   Instructor.create(id: "1", name: "Ed Gehringer", email: "e@ncsu.edu", password: "1", created_at: "2016-02-12 23:11:54.405191", updated_at: "2016-02-14 01:12:35.667767")
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Admin.create(id:'1', name: 'Pranav K', email: 'pkulkar5@ncsu.edu', password: '1', created_at: '2016-01-30 22:30:21.208673' , updated_at: '2016-02-17 01:47:08.667407')
->>>>>>> b1d01a764638c3ccfd637fa41f1aa74ed85000e1
+
